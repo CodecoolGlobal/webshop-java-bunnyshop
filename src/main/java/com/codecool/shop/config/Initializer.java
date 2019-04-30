00df3a646d22b88263821f70c2e32700982b16f9
Dániel Polgár <polgarda@gmail.com>
2019-04-30 11:09:24 +0200

    First 3 category updated

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -30,12 +30,16 @@ public class Initializer implements ServletContextListener {
         supplierDataStore.add(lenovo);
 
         //setting up a new product category
-        ProductCategory americanSable = new ProductCategory("American Sable", "animal", "The American Sable is a result of Chinchilla rabbit crosses. Sables are identical to Chinchilla rabbits in body conformation, but their coats are colored differently.");
-        productCategoryDataStore.add(americanSable);
+        ProductCategory alaska = new ProductCategory("Alaska", "animal", "Despite its name, the Alaska Rabbit originates in Germany, rather than Alaska.");
+        productCategoryDataStore.add(alaska);
+        ProductCategory angora = new ProductCategory("Angora","animal", "The Angora rabbit (Turkish: Ankara tavşanı), which is one of the oldest types of domestic rabbit, is bred for the long fibers of its coat, known as Angora wool, that are gathered by shearing, combing, or plucking.");
+        productCategoryDataStore.add(angora);
+        ProductCategory californian = new ProductCategory("Californian", "animal", "The Californian, also known as the California White, is a breed of domestic rabbit originally developed for the fur and meat industries by George S. West of Lynnwood, California, starting in 1923.");
+
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", americanSable, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", americanSable, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", americanSable, amazon));
+        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", alaska, amazon));
+        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", angora, lenovo));
+        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", californian, amazon));
     }
 }
