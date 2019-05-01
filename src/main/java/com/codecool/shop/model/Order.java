3146510bff88b37237bae2bea4581758f33b5a8d
rolandgonczi <gonczi.roland@gmail.com>
2019-05-01 13:17:37 +0200

    Order.java variables declared

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -4,4 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+
+    private static Order instance = null;
+
+    private List<LineItem> items = new ArrayList<>();
+    private int totalSum;
+    private final String CURRENCY = "Credits";
+
+
+    public Order(String orderName, String orderDescription) {
+        super(orderName, orderDescription);
+    }
+
 }
