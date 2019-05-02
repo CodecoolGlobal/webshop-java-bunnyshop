f2a1477148cb6a96935d0427e5c4c7d45c3813c4
Dániel Polgár <polgarda@gmail.com>
2019-05-02 10:53:26 +0200

    Header logo added, css modifications

--- src/main/webapp/static/scripts/payment.js
+++
@@ -1,9 +0,0 @@
-payment = {
-    cardNumberModifier: function(){
-        $('#cardNumber').keyup(function() {
-            $(this).val($(this).val().replace(/(\d{4})(\d+)/g, '$1-$2'))
-        });
-    }
-};
-
-payment.cardNumberModifier();
