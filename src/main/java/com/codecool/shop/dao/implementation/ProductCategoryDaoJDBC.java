38549b019f846250680042f05e296d2455ae0f42
Roland Gonczi <gonczi.roland@gmail.com>
2019-05-15 13:20:50 +0200

    ProductCategoryDaoJDBC getInstance() Singleton created

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJDBC.java
@@ -1,4 +1,32 @@
 package com.codecool.shop.dao.implementation;
 
-public class ProductCategoryDaoJDBC {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.database.dbCreator;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCategoryDaoJDBC implements ProductCategoryDao{
+
+    private static ProductCategoryDaoJDBC instance = null;
+
+    public ProductCategoryDaoJDBC() {}
+
+    public static ProductCategoryDaoJDBC getInstance() {
+        if (instance == null) {
+            instance = new ProductCategoryDaoJDBC();
+        }
+        return instance;
+    }
+
+
+
+
 }
