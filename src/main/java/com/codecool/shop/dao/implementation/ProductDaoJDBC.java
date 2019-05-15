6d741feda29c32b519322bf086db7b2ab46cc56c
Roland Gonczi <gonczi.roland@gmail.com>
2019-05-15 11:19:39 +0200

    ProductDaoJDBC constructor and getInstance() created, imports implemented

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -1,5 +1,38 @@
 package com.codecool.shop.dao.implementation;
 
-public class ProductDaoJDBC {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+import com.codecool.shop.database.dbCreator;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDaoJDBC implements ProductDao {
+
+    private static ProductDaoJDBC instance = null;
+
+    public ProductDaoJDBC() {
+
+    }
+
+    public static ProductDaoJDBC getInstance() {
+        if (instance == null) {
+            instance = new ProductDaoJDBC();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+
+    }
 
 }
